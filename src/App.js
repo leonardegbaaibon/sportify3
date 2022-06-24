d5fa36db82bf03a72a775db9e88367fcabf4282d
leonard egbaaibon <legbaaibon@gmail.com>
2022-06-24 05:14:01 +0100

    Updated the  search input

--- src/App.js
+++ src/App.js
@@ -6,9 +6,32 @@ import Navbar from "./Navbar";
 
 function App() {
   const [image, setImage] = useState([]);
+  const [searchItem, setSearchItem] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
 
   // const getApi = () => {
-
+  const searchHandler = (searchItem) => {
+    // console.log(searchItem)
+    setSearchItem(searchItem);
+    if (searchItem !== "") {
+      const newSet = image.filter((data) => {
+          return Object.values(data)
+            .join(" ")
+            .toLowerCase()
+            .includes(searchItem.toLowerCase())
+        ;
+      });
+      setSearchResult(newSet)
+      // console.log(searchResult)
+      // if(searchResult.length === 0){
+      //     console.log(searchWarning) 
+      //     return searchWarning
+      //   }
+      }
+      else{
+        setSearchResult(image)
+      }
+    };
   const options = {
     method: "GET",
     url: "https://free-football-soccer-videos.p.rapidapi.com/",
@@ -21,7 +44,7 @@ function App() {
     axios
       .request(options)
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
         setImage(res.data);
       })
       .catch(function (error) {
@@ -41,11 +64,17 @@ function App() {
   // }
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar 
+      term={searchItem}
+      searchKeyword={searchHandler}
+      searchWarn = {searchResult} />
       <header className="App-header">
         {/* <button onClick={getApi}><h3>Get react</h3></button> */}
-        {image.map((data) => (
-          <div className="arrange-div">
+        {searchResult.length === 0 && searchItem.length > 0 ? <div className="result-warning">No Results found</div>  : console.log()}
+        {searchItem.length > 0 ? (
+          searchResult.map((data) => {
+            return(            
+            <div className="arrange-div">
             <div key={data.competition.id} className="key-div">
               <h4 className="title-text">{data.title}</h4>
               <img src={data.thumbnail} alt="" />
@@ -58,7 +87,27 @@ function App() {
               </div>
             </div>
           </div>
-        ))}
+            )
+          })
+        ) : (
+          image.map((data) => {
+            return(            
+            <div className="arrange-div">
+            <div key={data.competition.id} className="key-div">
+              <h4 className="title-text">{data.title}</h4>
+              <img src={data.thumbnail} alt="" />
+              <div className="status">{data.videos[0].title}</div>
+              <div className="link-title">
+                <a href={data.embed.slice(137, 230)}>
+                  <FiPlayCircle />
+                </a>
+                <div className="date">Date: {data.date.slice(0, 10)}</div>
+              </div>
+            </div>
+          </div>
+            )
+          })
+        )}
       </header>
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,22 +1,37 @@
 import { FaSearch } from "react-icons/fa";
+import { useRef } from "react";
 
-// import { useState } from "react";
-// const [search, setSearch] = useState("")
+const Navbar = (props) => {
+//   console.log(props);
+  const inputEl = useRef("")
 
-const Navbar = () => {
-    return ( 
-        <div class="header1">
-        <div class="search-div">
-            <input type="text" id="search" required placeholder="Search by League...." />
-            <div class="icon-btn">
-                <FaSearch />
-            </div>
+  const getSearchItem = () => {
+    props.searchKeyword(inputEl.current.value)
+    // console.log(inputEl.current.value)
+  }
+  const searchClick = () => {
 
+  }
+
+  return (
+    <div class="header1">
+      <div class="search-div">
+        <input
+        ref={inputEl}
+          type="text"
+          id="search"
+          required
+          placeholder="Search by League...."
+          value={props.items}
+          onChange={getSearchItem}
+
+        />
+        <div class="icon-btn" onClick={searchClick}>
+          <FaSearch />
         </div>
-
-
+      </div>
     </div>
-     );
-}
- 
+  );
+};
+
 export default Navbar;
